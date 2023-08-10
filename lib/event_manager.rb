34e1fc63b2ecfa5afaf83690952af623c8ddf0f9
spalme21 <spalme21@gmail.com>
2023-08-09 21:07:14 -0500

    Finish initial program

---
+++ lib/event_manager.rb
@@ -0,0 +1,56 @@
+require "csv"
+require "google/apis/civicinfo_v2"
+require "erb"
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: "country",
+      roles: ["legislatorUpperBody", "legislatorLowerBody"]
+    ).officials
+  rescue
+    "You can find your representative"
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir("output") unless Dir.exist?("output")
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, "w") do |file|
+    file.puts form_letter
+  end
+end
+
+puts "Event Manager Initialize!"
+
+contents = CSV.open(
+  "event_attendees.csv", 
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read("form_letter.erb")
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
+end
