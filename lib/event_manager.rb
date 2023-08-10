6fbdc8f90d965ce3c3820c4f1a80ff0a4a0cce59
spalme21 <spalme21@gmail.com>
2023-08-09 21:21:20 -0500

    Add clean phone number mfunction

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,17 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
 
+def clean_phone_number(phone_number)
+  phone_number.gsub!(/[^\d]/,'')
+  if phone_number.length == 10
+    phone_number
+  elsif phone_number.length == 11 && phone_number[0] == "1"
+    phone_number[1..10]
+  else
+    "Bad Number"
+  end
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -48,9 +59,13 @@ contents.each do |row|
 
   zipcode = clean_zipcode(row[:zipcode])
 
+  phone = clean_phone_number(row[:homephone])
+
+  puts phone
+
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
-  save_thank_you_letter(id, form_letter)
+  #save_thank_you_letter(id, form_letter)
 end
